bebb483dca43ce164b9f7e3191d0ed15932621a2
MoisesRabelo <moisesramo48@gmail.com>
2020-09-06 09:40:56 -0300

    começo

---
+++ concord/src/test/java/br/com/concord/ConcordApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.concord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
