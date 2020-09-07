12438bcdd081dd35e6f49079cafa0a3c17b1720b
MoisesRabelo <moisesramo48@gmail.com>
2020-09-07 18:03:38 -0300

    register/login/logout

--- concord/src/main/java/br/com/concord/controller/UserController.java
+++ concord/src/main/java/br/com/concord/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController 
 {
 	@GetMapping("/")
-    public String home() {
-        return "home";
-    }
+	public String home() {
+		return "home";
+	}
 }
